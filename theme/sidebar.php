de97cf66e8a5867d4ff99e3a0d9101d8ef2a7173
Takosuke <solidparallel6890@gmail.com>
2017-02-10 18:30:01 +0100

    changed sidebar title to bloginfo()'

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -1,5 +1,5 @@
 		<div class="sidebar">
-			<h1 class="sidebar__header">Octavio Garabello</h1>
+			<h1 class="sidebar__header"><?php bloginfo( 'name' ) ?></h1>
 				<?php if ( has_nav_menu( 'primary' ) ) : ?>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'sidebar', 'menu_class' => 'sidebar__nav', 'menu_id' => '' ) ); ?>
 				<?php endif; ?>
